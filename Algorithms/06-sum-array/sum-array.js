4f71292ce700468694da9f4d884eb22f927bc522
Shahid Amin <schboostie@gmail.com>
2023-03-24 16:01:12 +0000

    Algorithums completed

--- Algorithms/06-sum-array/sum-array.js
+++ Algorithms/06-sum-array/sum-array.js
@@ -1,3 +1,10 @@
 // Write code to add all the numbers in `arr` and return the total
 
-var sumArray = function(arr) {};
+var sumArray = function (arr) {
+  let sumOne = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sumOne += arr[i];
+  }
+  return sumOne;
+};
